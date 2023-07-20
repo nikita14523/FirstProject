3e9a22296dd221fb4b41231abcc49a37c2b5ea30
nikita14523 <nikitagunjal484@gmail.com>
2023-07-20 15:19:03 +0530

    [nikita14523]Add New TC---> ClassB

---
+++ src/mypack/ClassB.java
@@ -0,0 +1,24 @@
+package mypack;
+
+import java.util.Arrays;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] b = new int[5];
+		
+		b[0] = 100;
+		b[1] = 200;
+		b[2] = 300;
+		b[3] = 400;
+		b[4] = 500;
+		
+		System.out.println(b[1]);
+		
+		System.out.println(Arrays.toString(b));
+		
+	}
+
+}
